5ccb35cf709de77fddec26fa42e138c5a9daf6ee
Khushi Sharma <sharmakhushi0313@gmail.com>
2024-07-18 05:14:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -101,8 +101,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
     function switchTheme(e) {
         if (e.target.checked) {
             document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
         } else {
             document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
         }
     }
 
